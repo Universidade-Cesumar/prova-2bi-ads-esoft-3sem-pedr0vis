34a9cbbe59a0b217cdb78ee40ab174a34e6e1be7
Pedro Henrique Oliveira Bezerra <pedro.11hob@gmail.com>
2026-06-10 19:30:48 -0300

    limpar e preencher formulario criados

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // Arquivo para código javascript
+
+
+
+const limparFormulario = () => {
+
+    document.getElementById('input-nome').value = '';
+    document.getElementById('input-quantidade').value = '';
+
+}
+
+const preencherFormulario = (cadastro) => {
+
+    document.getElementById('input-nome').value = cadastro.nomeMaterial;
+    document.getElementById('input-quantidade').value = cadastro.inputQuantidade;
+
+}
